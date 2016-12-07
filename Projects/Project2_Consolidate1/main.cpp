95238f99cfca5879df3ea33724b0fecdf65704d8
Unknown <thomasjeffrey96@hotmail.com>
2016-12-07 10:36:54 -0800

    12/07/16
    Fixed Scoring Problem

--- Projects/Project2_Consolidate1/main.cpp
+++ Projects/Project2_Consolidate1/main.cpp
@@ -29,9 +29,10 @@ void dispBrd(char[][7],int);
 
 //Scoreboard functions
 //Assign new 1D array values to 2D array
-void updtScr(int[][10],int[],bool[],int,int&);
+void updtScr(int[][FRAME],int[],bool[],int,int&);
 //Output scoreboard function
-void dispScr(int[][FRAME],int);
+void dispScr(int[][FRAME],string[],int);
+void dispScr1D(int[],int);
 
 //Array manipulation functions
 //Randomly assign bool value for each pin
@@ -54,19 +55,30 @@ int main(int argc, char** argv)
     int totCont=0; //Total number of pins knocked down in one frame
     
     //Input number of players
-    cin>>utilize;
+    do{
+        cout<<"Input the number of players (1 - 4)"<<endl;
+        cin>>utilize;
+    }while(utilize<1||utilize>4);
+    
     
     //Declare Arrays
-    int  rawScor[utilize*10]={};     //Holds frame scores for all players
-    bool isPassd[utilize*10]={};//Holds whether frame has been completed
-    int  dspScor[utilize][FRAME]={}; //"Parallel" array to rawScor
-    bool isKnok[PIN]={};             //Holds standing/fallen status of pins
-    char pins[PIN]={};               //Displays a standing or fallen pin
-    char board[4][7];                //Displays entire 10 pin board
+    int    rawScor[utilize*FRAME]={};  //Holds frame scores for all players
+    bool   isPassd[utilize*FRAME]={};  //Holds whether frame has been completed
+    int    dspScor[utilize][FRAME]={}; //"Parallel" array to rawScor
+    bool   isKnok[PIN]={};             //Holds standing/fallen status of pins
+    char   pins[PIN]={};               //Displays a standing or fallen pin
+    char   board[4][7];                //Displays entire 10 pin board
+    string names[utilize];             //Names of the players
+    
+    //Declare player names
+    for(int i=0;i<utilize;i++){
+        cout<<"Name of Player "<<i+1<<": ";
+        cin>>names[i];
+    }
     
     //Output Scoreboard
     cout<<"Scores"<<endl;
-    dispScr(dspScor,utilize);
+    dispScr(dspScor,names,utilize);
     cout<<endl;
 
     //Display before any throws
@@ -78,27 +90,29 @@ int main(int argc, char** argv)
     dispBrd(board,7);
     cout<<endl<<endl;
     
-    for(int i=0;i<40;i++){
+    for(int i=0;i<utilize*FRAME;i++){
         isPassd[i]=1;
     }
     
-    for(int i=0;i<40;i++){
+    cout<<"Bool Array"<<endl;
+    for(int i=0;i<utilize*FRAME;i++){
         cout<<isPassd[i]<<' ';
         if(i%10==9)cout<<endl;
     }
     cout<<endl;
-
-    for(int i=0;i<40;i++){
+    
+    int count=1;
+    do{
         //Take first throw (P1)
-        toss(isKnok,PIN,totCont);
-
+        toss(isKnok,PIN,totCont);        
+        
         //Update score and board
         updtScr(dspScor,rawScor,isPassd,utilize,totCont);
         cout<<"Scores"<<endl;
-        dispScr(dspScor,utilize);
+        dispScr(dspScor,names,utilize);
         cout<<endl;
 
-        cout<<"Roll "<<i+1<<endl;
+        cout<<"Roll "<<count<<endl;
         prntBrd(isKnok,pins,PIN);
         cout<<endl<<endl;
 
@@ -106,7 +120,16 @@ int main(int argc, char** argv)
         dispBrd(board,7);
         cout<<endl<<endl;
         reset(isKnok,PIN);
+        totCont=0;
+        count++;
+    }while(count<=utilize*FRAME);
+    
+    cout<<"Bool Array"<<endl;
+    for(int i=0;i<utilize*FRAME;i++){
+        cout<<isPassd[i]<<' ';
+        if(i%10==9)cout<<endl;
     }
+    cout<<endl;
     
     //Exit program
     return 0;
@@ -114,23 +137,25 @@ int main(int argc, char** argv)
 
 //Pin display functions
 void prntBrd(bool isKnok[],char pins[],int size){
+    //Check if pins is standing or fallen
     for(int i=0;i<size;i++){
         isKnok[i]%2==0?pins[i]='o':pins[i]='x';
     }
+    //Output pin status
     for(int i=0;i<size;i++){
         cout<<pins[i]<<' ';
     }
 }
 
 void fillBrd(char board[][7],char pins[],int size){
-    int k=0;
+    int k=0; //Third nested loop counter
     for(int i=0;i<4;i++){
-        for(int j=0;j<size;j++){
-            
-            board[i][j]='-';
+        for(int j=0;j<size;j++){            
+            board[i][j]='-'; //Set all tiles on board to '-'
             if(i==j%2||i==j||(i%2==0&&j==i+2)){
+                //Change tile if it is in the position of one of the pins
                 board[i][j]=pins[k];
-                k++;
+                k++; //Increment counter
             }
             
         }
@@ -147,71 +172,55 @@ void dispBrd(char board[][7],int size){
 }
 
 //Scoreboard Functions
-void updtScr(int dspScor[][10],int rawScor[],bool isPassd[],int utilize,int &totCont){
-    //int count=0;
-/*
- *
-    for(int i=0;i<10;i++){
-        for(int j=0;j<utilize;j++){
-            if(isPassd[(i*utilize)+j]!=0){
-                rawScor[(i*utilize)+j]=totCont;
-                isPassd[(i*utilize)+j]=0;
-                totCont=0;
-                break;
-            }
-            //totCont=0;
-        }
-        //totCont=0;
-    }
-*/
-    for(int i=0;i<utilize*10;i++){
-        if(isPassd[i]==0){
-            rawScor[i]=rawScor[i];
-        }
-        if(isPassd[i]!=0){
-            rawScor[i]=totCont;
-            isPassd[i]=true;
-            totCont=0;
+void updtScr(int dspScor[][FRAME],int rawScor[],bool isPassd[],int utilize,int &totCont){
+    for(int i=0;i<utilize*10;i++){  //Set counter to increment for # of frames
+        if(isPassd[i]!=0){          //Check if frame has passed yet
+            rawScor[i]=totCont;     //Update score
+            isPassd[i]=0;           //Indicate that frame has now passed
+            totCont=0;              //Reset score
+            break;                  //End loop
         }
     }
-
+    
     for(int i=0;i<10;i++){
         for(int j=0;j<utilize;j++){
-            dspScor[j][i]=rawScor[(i*utilize)+j];
+            dspScor[j][i]=rawScor[(i*utilize)+j]; //Assign 1D value to 2D array
         }
     }
 }
 
-
-
-
-
-
-void dispScr(int dspScor[][10],int utilize){
+//2D Array Display
+void dispScr(int dspScor[][FRAME],string names[],int utilize){        
     for(int i=0;i<utilize;i++){;
+    cout<<names[i]<<" | ";            //Output player names in front of scores
         for(int j=0;j<10;j++){
-            cout<<dspScor[i][j]<<' ';
-            if(j%10==9)cout<<endl;
+            cout<<dspScor[i][j]<<' '; //Output scores
+            if(j%10==9)cout<<endl;    //Line break after 10 frames
         }
     }
 }
 
 //Array manipulation functions
 void toss(bool isKnok[],int size,int &totCont){
-    int curCont=0;
+    int curCont=0;               //Score of current toss
     for(int i=0;i<size;i++){
-        if(isKnok[i]!=1){
-            isKnok[i]=rand()%2;
-            if(isKnok[i]==1){
-                curCont++;
+        if(isKnok[i]!=1){        //Check if pin has been knocked down yet
+            isKnok[i]=rand()%2;  //50% chance of knocking down unknocked pin 
+            if(isKnok[i]==1){    //Check if any new pins have been knocked down
+                curCont++;       //Add to throw total
             }
         }
     }
-    totCont+=curCont;
+    totCont+=curCont;            //Add to frame total
 }
 
 void reset(bool isKnok[],int size){
     for(int i=0;i<size;i++){
-        isKnok[i]=0;
+        isKnok[i]=0;         //Set all pins back to standing
     }
+}
+
+//Totaling functions
+int ttlScor(int dspScor[][FRAME],string names[]){
+    
 }
